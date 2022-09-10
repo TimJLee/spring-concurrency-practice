917a854f1ff38ca9bc4747d9d091e832275d7394
TimJLee <jyonglee95@gmail.com>
2022-09-10 18:19:18 +0900

    stock 서비스 구성

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,27 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StockService {
+    private StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // get stock
+        // 재고 감소
+        // 저장
+
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+        // saveAndFlush: 영속성 컨텍스트가 아닌 db 에 바로 값을 저장.
+        stockRepository.saveAndFlush(stock);
+    }
+}
