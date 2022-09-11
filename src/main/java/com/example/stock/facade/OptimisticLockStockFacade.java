5f80fd458e033afe2ba38b2abdcf61114d47ee6b
TimJLee <jyonglee95@gmail.com>
2022-09-11 16:46:28 +0900

    OptimisticLock 으로 동시성 문제 해결

---
+++ src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,26 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.OptimisticLockStockService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OptimisticLockStockFacade {
+
+    private OptimisticLockStockService optimisticLockStockService;
+
+    public OptimisticLockStockFacade(OptimisticLockStockService optimisticLockStockService) {
+        this.optimisticLockStockService = optimisticLockStockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (true) {
+            try {
+                optimisticLockStockService.decrease(id, quantity);
+                break;
+            } catch (Exception e) {
+                // 실패했을 때 50millsecond 자고 후에 다시 시도하도록 하는 코드
+                Thread.sleep(50);
+            }
+        }
+    }
+}
